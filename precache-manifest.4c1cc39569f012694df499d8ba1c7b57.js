e1ef29839fc51399c9a1dede568a9074020fe13f
kapiljhajhria <Aerospace44>
2020-05-14 11:45:26 +0530

    Updates

---
+++ precache-manifest.4c1cc39569f012694df499d8ba1c7b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0018260480583a2ef7ee49153d39a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70abe3e246ac936d7ef8",
+    "url": "./static/css/main.ef8b13d4.chunk.css"
+  },
+  {
+    "revision": "7e898c654bce3b9e0f03",
+    "url": "./static/js/2.1c43b6d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1c43b6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70abe3e246ac936d7ef8",
+    "url": "./static/js/main.c3793615.chunk.js"
+  },
+  {
+    "revision": "7bcd35c14b0272dfa2da",
+    "url": "./static/js/runtime-main.79ea6177.js"
+  }
+]);
