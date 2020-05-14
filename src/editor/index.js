352f0deaba2db19032f49b351ec95185b50c5995
kapiljhajhria <Aerospace44>
2020-05-14 13:53:50 +0530

    able to render string as html

--- src/editor/index.js
+++ src/editor/index.js
@@ -5,12 +5,24 @@ import React, {useEffect, useState} from 'react';
 export default function Editor(props) {
     const [textAreaValue, setTextAreaValue] = useState("")
 
+    let testString = 'hi there how are you?  <h3 style="color: red"> h2 heading </h3>'
+
+
+    // let testString =
+
+
     function handleChange(e) {
         setTextAreaValue(e.target.value);
     }
 
     function txtToMarkDown(txt) {
-        return txt + "\nconverted text"
+        let res = textAreaValue.replace(/^([#][#] )(.*)/gm, (m,g)=>`{<h2>hi</h2>}`)
+        return res
+    }
+    function htmlDecode(input){
+        var e = document.createElement('div');
+        e.innerHTML = input;
+        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
     }
     useEffect(() => {
         console.log("useffect called");
@@ -22,11 +34,11 @@ export default function Editor(props) {
             <textarea className={"txtArea"}
                       value={textAreaValue}
                       onChange={handleChange}
-                      textAreaValue
             />
 
             <div className="txtOutput">
-                {txtToMarkDown(textAreaValue)}
+                {/*<div dangerouslySetInnerHTML={{ __html: htmlDecode(testString) }} />*/}
+                {React.createElement("p", {dangerouslySetInnerHTML: {__html: testString}})}
             </div>
         </div>
     )
